dd68121843a77344e6937e44f0e589275074f91b
HepenkoOleksandra <gepenkoalex@gmail.com>
2024-03-15 23:17:42 +0200

    start

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+// import { useState } from 'react'
+import Description from './Description/Description'
+import Options from './Options/Options'
+import Feedback from './Feedback/Feedback'
+
+
+function App() {
+  
+
+  return (
+    <>
+    <Description />
+    <Options/>
+    <Feedback/>
+    </>
+  )
+}
+
+export default App
---
+++ src/Options/Options.jsx
@@ -0,0 +1,10 @@
+
+
+
+const Options = () => {
+  return (
+    <div>Options</div>
+  )
+}
+
+export default Options
